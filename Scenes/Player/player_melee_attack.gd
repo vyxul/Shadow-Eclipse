9519f1716d56633cc1fe350790ee24e3c101932d
vyxul <stevendotran@gmail.com>
2024-01-15 19:35:58 -0600

    Created player melee attack and hitbox component

---
+++ Scenes/Player/player_melee_attack.gd
@@ -0,0 +1,31 @@
+extends Node2D
+
+const extension_range = 32
+
+var weapon_damage: int = 0
+
+
+#func _process(delta):
+	#set_attack_position()
+
+
+func set_damage(dmg: int):
+	weapon_damage = dmg
+
+
+func get_damage():
+	return weapon_damage
+
+
+func set_attack_position():
+	var player = get_tree().get_first_node_in_group("player")
+	var player_position = player.get_weapon_origin() as Vector2
+	var weapon_direction = player_position.direction_to(get_global_mouse_position())
+	var weapon_position = player_position + (weapon_direction * extension_range)
+	var weapon_angle = weapon_direction.angle()
+	global_position = weapon_position
+	rotation = weapon_angle + (PI/2)
+
+func attack():
+	set_attack_position()
+	$AnimationPlayer.play("attack")
---
+++ Scenes/Player/player_melee_controller.gd
@@ -0,0 +1,11 @@
+extends Node
+
+@onready var player_melee_attack = preload("res://Scenes/Player/player_melee_attack.tscn")
+
+@export var weapon_damage: int = 1
+
+func attack(player_sprite: Node):
+	var player_melee_attack_instance = player_melee_attack.instantiate()
+	player_sprite.add_child(player_melee_attack_instance)
+	player_melee_attack_instance.set_damage(weapon_damage)
+	player_melee_attack_instance.attack()
