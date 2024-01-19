d3171edc6aae377bb45e89e09583348759e97f97
Nikos Fra <fragkasnikolaos@gmail.com>
2024-01-19 14:02:40 +0000

    Start Screen and flow
    Added a Loading Screen, Start Screen And basic UI

---
+++ GameFiles/Game Objects/UI Object/Button/UI Button.gd
@@ -0,0 +1,12 @@
+extends BaseButton
+
+const loading_scene_path = "res://GameFiles/Game Objects/UI Object/LoadingScreen/LoadingScreen.tscn"
+
+func _ready():
+	var button = Button.new()
+	button.pressed.connect(self._on_pressed)
+	add_child(button)
+
+func _on_pressed():	
+	print ("Pressed")
+	get_tree().change_scene_to_file(loading_scene_path)
