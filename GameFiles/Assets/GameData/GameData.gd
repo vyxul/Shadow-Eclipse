1f23ab43f29e238d9f449beb8253d25f530e87e8
Nikos Fra <fragkasnikolaos@gmail.com>
2024-01-20 02:34:29 +0000

    Step 1 Save Load Settings
    Added save and load functions

---
+++ GameFiles/Assets/GameData/GameData.gd
@@ -0,0 +1,25 @@
+extends Node
+
+class_name  GameSettings
+
+static var MusicVolume:float = 10
+static var SFXVolume:float = 10
+static var VoiceVolume:float = 10
+
+static func SetMusicVolume(volume) -> void:
+	MusicVolume = volume
+
+static func SetSFXVolume(volume) -> void:
+	SFXVolume = volume
+
+static func SetVoiceVolume(volume) -> void:
+	VoiceVolume = volume
+
+static func GetMusicVolume() -> float:
+	return MusicVolume
+
+static func GetSFXVolume() -> float:
+	return SFXVolume
+
+static func GetVoiceVolume() -> float:
+	return VoiceVolume
