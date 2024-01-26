b5f4eed8e7786058aa41925f3006686234417459
Nikos Fra <fragkasnikolaos@gmail.com>
2024-01-26 23:34:23 +0000

    fountain

--- GameFiles/Scenes/UI/defeat_screen.gd
+++ GameFiles/Scenes/UI/defeat_screen.gd
@@ -2,18 +2,12 @@ extends CanvasLayer
 
 @onready var score_value_label = $ColorRect/MarginContainer/VBoxContainer/HBoxContainer/ScoreValueLabel
 @onready var continue_button = $ColorRect/MarginContainer/HBoxContainer/ContinueButton
-<<<<<<< HEAD
 @onready var ReasonLabel = $ColorRect/MarginContainer/VBoxContainer/ReasonLabel
-=======
-@onready var quit_button = $ColorRect/MarginContainer/HBoxContainer/QuitButton
-
->>>>>>> parent of 6f0780c (Merge branch 'main' into Timer-and-Spawns)
 var Game = "res://GameFiles/Scenes/Menu/StartScreen.tscn"
 
 func _ready():
 	visible = false
 	continue_button.disabled = true
-	quit_button.disabled = true
 
 
 func get_score() -> int:
@@ -29,7 +23,6 @@ func appear(Reason : int = 0):
 	
 	visible = true
 	continue_button.disabled = false
-	quit_button.disabled = false
 	score_value_label.text = str(get_score())
 	GameState.CalculateAndIncreaseDarknessFromScore(1)
 	get_tree().paused = true
@@ -38,7 +31,6 @@ func appear(Reason : int = 0):
 func disappear():
 	visible = false
 	continue_button.disabled = true
-	quit_button.disabled = true
 	get_tree().paused = false
 
 
@@ -47,7 +39,3 @@ func _on_continue_button_pressed():
 	GameData.reset()
 	GameState.reset()
 	LoadManager.load_scene(Game)
-
-
-func _on_quit_button_pressed():
-	get_tree().quit()
